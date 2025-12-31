19bb8f1fb4759d94ea11ffc68cfac9d468c3f3cd
TakeSomen99 <s24c1117ug@s.chibakoudai.jp>
2026-01-01 01:50:23 +0900

    modify test items

--- src/mypkg/test/test.bash
+++ src/mypkg/test/test.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -
+set -e
 
 WS_DIR=$(cd "$(dirname "$0")/../.." && pwd)
 cd "$WS_DIR"
@@ -8,10 +8,13 @@ echo "[1] colcon build"
 colcon build --packages-select device_msgs mypkg
 source install/setup.bash
 
-echo "[2] launch test"
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+echo "[2] launch smoke test"
+timeout 5 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log 2>&1 || true
 
-grep 'ELECOM' /tmp/mypkg.log
+echo "[3] check launch error"
+if grep -Ei "error|traceback|failed" /tmp/mypkg.log; then
+  echo "Launch failed"
+  exit 1
+fi
 
 echo "test was completed!!!"
-
