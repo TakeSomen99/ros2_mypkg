e75f31dcede335877e91951960e670d96e78cf66
TakeSomen99 <s24c1117ug@s.chibakoudai.jp>
2025-12-31 23:42:21 +0900

    modify test items

--- src/mypkg/test/test.bash
+++ src/mypkg/test/test.bash
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -
 
-echo "=== ROS2 integration test start ==="
-
-WS=~/ros2_ws
-mkdir -p $WS/src
-cp -r . $WS/src/ros2_mypkg
-
-cd $WS
+WS_DIR=$(cd "$(dirname "$0")/../.." && pwd)
+cd "$WS_DIR"
 
 echo "[1] colcon build"
 colcon build --packages-select device_msgs mypkg
-
 source install/setup.bash
 
 echo "[2] launch test"
-timeout 10 ros2 launch mypkg talk_listen.launch.py > launch.log 2>&1 &
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-sleep 3
+grep 'ELECOM' /tmp/mypkg.log
 
-echo "[3] service existence test"
-ros2 service list | grep get_device_names
-
-echo "[4] service call test"
-ros2 service call /get_device_names device_msgs/srv/Device "{}" \
-  | grep "\["
-
-echo "=== ROS2 integration test SUCCESS ==="
+echo "test was completed!!!"
 
